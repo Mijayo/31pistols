b24151833f892b69d50dea83dd5b8ef322af792c
Diego <48282657+Mijayo@users.noreply.github.com>
2020-03-03 20:14:38 +0100

    _testDiego_V04

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,5 +13,9 @@ router.get('/contacto', (req, res) => {
     res.render('contacto');
 });
 
+// router.get('/login', (req, res) => {
+//     res.render('login');
+// });
+
 
 module.exports = router;
